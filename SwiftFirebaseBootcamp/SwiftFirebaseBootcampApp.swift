427934f33819b41bfb21ae9013014f6dc846f83a
Juan David <juandaguti@icloud.com>
2025-09-18 14:23:18 +0100

    Initial Commit

---
+++ SwiftFirebaseBootcamp/SwiftFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseBootcampApp.swift
+//  SwiftFirebaseBootcamp
+//
+//  Created by Juan David Gutierrez Olarte on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
